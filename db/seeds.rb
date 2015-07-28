29c5a6bb86b3a978a68d516a40744b3cdfe497ec
neudabei <rschultzejena@gmail.com>
2015-07-28 14:15:04 +0100

    initial setup via precourse

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Video.create(title: "The Wire", description: "One of the best TV shows ever made.", large_cover_url: "wire.jpg")
+Video.create(title: "Sopranos", description: "A very entertaining and well made TV show.", large_cover_url: "sopranos.jpg")
+Video.create(title: "The Simpsons", description: "A classic that needs no description.", large_cover_url: "simpsons")
