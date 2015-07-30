3882318fef469d7315a47f90a813b31f564262fd
neudabei <rschultzejena@gmail.com>
2015-07-30 12:32:33 +0100

    implemented search feature the tdd way

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,12 @@
 class Video < ActiveRecord::Base
   belongs_to :category
-
   validates_presence_of :title, :description
+
+  def self.search_by_title(search_term)
+    return [] if search_term.blank?
+    where("title LIKE ?", "%#{search_term}%").order("created_at DESC")
+    # whatever we pass in the part after the comma(,) replaces the question mark(?) and forms the SQL query
+    # % before and after search_term are wildcard paramaters
+  end
+
 end
