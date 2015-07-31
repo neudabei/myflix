918aa1e102e6f05986c0c0d090161c2464dd415f
neudabei <rschultzejena@gmail.com>
2015-07-31 13:19:22 +0100

    return recent_videos and test it works

--- spec/model/category_spec.rb
+++ spec/model/category_spec.rb
@@ -2,6 +2,42 @@ require 'spec_helper'
 
 describe Category do 
 
+  describe "#recent_videos" do # #sign when we test instance methods
+
+    it "returns the videos in the reverse chronical order by created_at" do
+      comedies = Category.create(name: "comedies")
+      futurama = Video.create(title: "Futurama", description: "Space Travel", category: comedies, created_at: 1.day.ago)
+      southpark = Video.create(title: "Southpark", description: "Crazy kids", category: comedies)
+      expect(comedies.recent_videos).to eq([southpark, futurama])
+    end
+
+    it "returns all the videos if there are less than 6 videos" do
+      comedies = Category.create(name: "comedies")
+      futurama = Video.create(title: "Futurama", description: "Space Travel", category: comedies, created_at: 1.day.ago)
+      southpark = Video.create(title: "Southpark", description: "Crazy kids", category: comedies)
+      expect(comedies.recent_videos.count).to eq(2)
+    end
+
+    it "returns 6 videos if there are more than 6 videos" do
+      comedies = Category.create(name: "comedies")
+      7.times { Video.create(title: "foo", description: "bar", category: comedies) }
+      expect(comedies.recent_videos.count).to eq(6)
+    end
+
+    it "returns the most recent 6 videos" do
+      comedies = Category.create(name: "comedies")
+      6.times { Video.create(title: "foo", description: "bar", category: comedies)}
+      tonights_show = Video.create(title: "Tonights show", description: "talk show", category: comedies, created_at: 1.day.ago)
+      expect(comedies.recent_videos).not_to include(tonights_show)
+    end
+
+    it "returns an empty array if the category does not have any videos" do
+      comedies = Category.create(name: "comedies")
+      expect(comedies.recent_videos).to eq([])
+    end
+
+  end
+
   it {should have_many(:videos)}
   
   # it "saves itself" do
