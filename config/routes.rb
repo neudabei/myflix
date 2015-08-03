768a7747373f1432f77d2108ba28f501f8dcb974
neudabei <rschultzejena@gmail.com>
2015-08-03 10:00:47 +0100

    link up registration login pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Myflix::Application.routes.draw do
-  root to: "videos#index"
-  resources :videos, only: [:show] do
+  root to: "users#front"
+  resources :videos, only: [:show, :index] do
     collection do
       get :search, to: "videos#search"
     end
@@ -9,5 +9,13 @@ Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
   get '/genre/:id', to: 'categories#show'
+
+  # registration/authentication
+  get '/register', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  resources :users, only: [:show, :create, :edit, :update]
 
 end
