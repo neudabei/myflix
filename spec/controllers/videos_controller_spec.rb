ad1bc804cca2081a5f48df246263475a0dc49dcd
neudabei <rschultzejena@gmail.com>
2015-08-04 21:46:15 +0100

    write videos controller tests

---
+++ spec/controllers/videos_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe VideosController do
+  describe "GET show" do
+    context "with authenticated users" do
+      before do
+        session[:user_id] = Fabricate(:user).id #to log us in for running the test
+      end
+      it "sets the @video variable" do
+        video = Fabricate(:video)
+        get :show, id: video.id
+        expect(assigns(:video)).to eq(video)
+      end
+    end
+    context "with unauthenticated users" do
+      it "redirects the user to the sign in page" do
+        video = Fabricate(:video)
+        get :show, id: video.id
+        expect(response).to redirect_to login_path
+      end
+    end
+  end
+  describe "GET search" do
+    it "sets @results for authenticated users" do
+      session[:user_id] = Fabricate(:user).id
+      futurama = Fabricate(:video, title: "Futurama")
+      get :search, search_term: 'rama'
+      expect(assigns(:results)).to eq([futurama])
+    end
+    it "redirects to sign in page for the unauthenticated users" do
+      futurama = Fabricate(:video, title: "Futurama")
+      get :search, search_term: 'rama'
+      expect(response).to redirect_to login_path
+    end
+  end
+end
