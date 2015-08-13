f638ff10c5ed3ec3cf358fc5b6549c6d8f901053
neudabei <rschultzejena@gmail.com>
2015-08-13 15:57:17 +0100

    user can change video ratings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
-  def rate_video
-    options_for_select([5,4,3,2,1].map {|number| [pluralize(number, "Star")]})
+  def rate_video(selected=nil)
+    options_for_select([5,4,3,2,1].map {|number| [pluralize(number, "Star")]}, selected)
   end
   
 end
--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -12,14 +12,24 @@ class QueueItem < ActiveRecord::Base
     review.rating if review
   end
 
+  def rating=(new_rating)
+    review = Review.where(user_id: user.id, video_id: video.id).first
+    if review
+      review.update_column(:rating, new_rating)
+    else
+      review = Review.create(user: user, video: video, rating: new_rating)
+      review.save(validate: false)
+    end
+  end
+
   def category_name
     category.name
   end
 
-  def normalize_queue_item_positions
-    queue_items.each_with_index do |queue_item, index|
-      queue_item.update_attributes(position: index+1)
-    end
+  private
+
+  def review
+    @review ||= Review.where(user_id: user.id, video_id: video.id).first
   end
 
 end
