8c176e4b20f140c86886e59622c71c6ed8b4076b
neudabei <rschultzejena@gmail.com>
2015-09-21 12:43:35 +0100

    Write tests for email sending

--- config/application.rb
+++ config/application.rb
@@ -5,12 +5,13 @@ require 'rails/all'
 Bundler.require(:default, Rails.env)
 
 # preload tokens in local_env.yml to local ENV
-config.before_configuration do
-  env_file = File.join(Rails.root, 'config', 'local_env.yml')
-  YAML.load(File.open(env_file)).each do |key, value|
-    ENV[key.to_s] = value
-  end if File.exists?(env_file)
-end
+
+# config.before_configuration do
+#   env_file = File.join(Rails.root, 'config', 'local_env.yml')
+#   YAML.load(File.open(env_file)).each do |key, value|
+#     ENV[key.to_s] = value
+#   end if File.exists?(env_file)
+# end
 
 module Myflix
   class Application < Rails::Application
