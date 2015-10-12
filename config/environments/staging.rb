925cfe9887723d7a3e4537eb20723a7a049ac7b2
neudabei <rschultzejena@gmail.com>
2015-10-12 22:53:54 +0100

    add staging.rb file to fix issue on staging server using production.rb file

---
+++ config/environments/staging.rb
@@ -0,0 +1,35 @@
+Myflix::Application.configure do
+
+  config.cache_classes = true
+  config.eager_load = true
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+
+  config.serve_static_assets = false
+
+  config.assets.compress = true
+  config.assets.js_compressor = :uglifier
+
+  config.assets.compile = false
+
+  config.assets.digest = true
+
+  config.i18n.fallbacks = true
+
+  config.active_support.deprecation = :notify
+
+
+  config.action_mailer.default_url_options = { host: 'staging-myflix-092015.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.mandrillapp.com',
+    port:                 587,
+    domain:               'myflix-092015.herokuapp.com',
+    user_name:            ENV["mandrill_username"],
+    password:             ENV['mandrill_api_key'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+  
+end
