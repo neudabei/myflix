880f3621c77d17dcda6ab8e7b76db1db11a15eaa
neudabei <rschultzejena@gmail.com>
2015-11-02 18:32:01 +0000

    implement admin video create action with controller tests

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -6,6 +6,17 @@ class Admin::VideosController < ApplicationController
     @video = Video.new
   end
 
+  def create
+    @video = Video.new(video_params)
+    if @video.save
+      flash[:success] = "You have successfully added the video #{@video.title}"
+      redirect_to new_admin_video_path
+    else 
+      flash[:error] = "The video could not be added. Please check the errors below."
+      render :new
+    end
+  end
+
   private
 
   def require_admin
@@ -13,5 +24,9 @@ class Admin::VideosController < ApplicationController
       flash[:error] = "You are not authorized to perform this action."
       redirect_to home_path 
     end
+  end
+
+  def video_params
+    params.require(:video).permit(:title, :description, :small_cover_url, :large_cover_url, :category_id)
   end
 end
--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -5,3 +5,11 @@ shared_examples "requires sign in" do
     expect(response).to redirect_to login_path
   end
 end
+
+shared_examples "requires admin" do
+  it "redirects to the home path" do
+    session[:user_id] = Fabricate(:user).id # kevin doesn't call .id here
+    action
+    expect(response).to redirect_to home_path
+  end
+end
