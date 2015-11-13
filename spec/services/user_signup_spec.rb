7a1409781df45bd8b11a0feaa1fae230ea8da854
neudabei <rschultzejena@gmail.com>
2015-11-13 11:28:42 +0000

    create a subscription plan and customers with stripe

--- spec/services/user_signup_spec.rb
+++ spec/services/user_signup_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe UserSignup do
   describe "#sign_up" do
     context "valid personal info and valid card" do
-      let(:charge) { double(:charge, successful?: true) }
-      before { StripeWrapper::Charge.should_receive(:create).and_return(charge) }
+      let(:customer) { double(:customer, successful?: true) }
+      before { StripeWrapper::Customer.should_receive(:create).and_return(customer) }
       after { ActionMailer::Base.deliveries.clear }
 
       it "creates a user" do
@@ -56,8 +56,8 @@ describe UserSignup do
 
     context "valid personal info and declined card" do
       it "does not create a new user record" do
-        charge = double(:charge, successful?: false, error_message: "Your card was declined.")
-        StripeWrapper::Charge.should_receive(:create).and_return(charge)
+        customer = double(:customer, successful?: false, error_message: "Your card was declined.")
+        StripeWrapper::Customer.should_receive(:create).and_return(customer)
         UserSignup.new(Fabricate.build(:user)).sign_up('1231241', nil)
         expect(User.count).to eq(0)
       end
@@ -71,7 +71,7 @@ describe UserSignup do
       end
 
       it "does not charge the card" do
-        StripeWrapper::Charge.should_not_receive(:create)
+        StripeWrapper::Customer.should_not_receive(:create)
       end
 
       it "does not send out the email when inputs are invalid" do
