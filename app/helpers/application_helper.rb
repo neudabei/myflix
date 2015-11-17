15b282315c8f5c0657638a9902e234d9a1010d8a
neudabei <rschultzejena@gmail.com>
2015-11-17 14:36:59 +0000

    extract 'rating selection options' into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,5 @@
 module ApplicationHelper
-
-  # def rate_video(selected=nil)
-  #   options_for_select([5,4,3,2,1].map {|number| [pluralize(number, "Star")]}, selected)
-  # end
-
-
-  def rate_video(selected=nil)
+ def rate_video(selected=nil)
     options_for_select([5,4,3,2,1].map {|number| [pluralize(number, "Star"), number]}, selected)
   end
 
@@ -13,4 +7,7 @@ module ApplicationHelper
     (src="http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(person)}?s=40")
   end
 
+  def rating_selection_options
+    (10..50).map {|num| num / 10.0}
+  end
 end
